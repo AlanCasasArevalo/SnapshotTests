65c94eae3546969e9e4aa9e0d0d3b135d7755e7f
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-12-21 19:02:29 +0100

    First commit

--- SnapshotTests/Views/ViewController.swift
+++ SnapshotTests/Views/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  SnapshotTests
-//
-//  Created by Tecdata Engineering on 21/12/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
