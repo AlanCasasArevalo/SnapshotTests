26ffa68b104ac39413416aeb57abf1ee16e36a21
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-12-21 19:37:10 +0100

    Function to record an image from initial state of view controller

--- SnapshotTestsTests/Views/SnapshotTestsTests.swift
+++ SnapshotTestsTests/Views/SnapshotTestsTests.swift
@@ -5,19 +5,43 @@ final class SnapshotTestsTests: XCTestCase {
     func test_zer() {
         let (sut) = makeSUT()
         
-        let snapshot = sut.snapshot()
+        record(snapshot: sut.snapshot(), named: "DEFAULT_INITIAL_SCREEN")
     }
 }
 
 extension SnapshotTestsTests {
     // MARK: - Helper
     func makeSUT () -> (ViewController) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let bundle = Bundle(for: ViewController.self)
+        let storyboard = UIStoryboard(name: "Main", bundle: bundle)
         let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
         
         sut.loadViewIfNeeded()
         
         return (sut)
+    }
+    
+    func record(snapshot: UIImage, named name: String, file: StaticString = #filePath, line: UInt = #line) {
+        guard let snapshotData = snapshot.pngData() else {
+            XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+            return
+        }
+
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+
+        do {
+            try FileManager.default.createDirectory(
+                at: snapshotURL.deletingLastPathComponent(),
+                withIntermediateDirectories: true
+            )
+
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
     }
 }
 
